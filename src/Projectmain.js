28204e1fa24824a63ce7bc6236e074ba6f277091
Arpreet Singh Arora <ar286506@dal.ca>
2022-02-23 19:20:14 -0400

    Project url

---
+++ src/Projectmain.js
@@ -0,0 +1,6 @@
+function Deploying() {
+
+    return <h1>Deploying project on heroku</h1>
+}
+
+export default Deploying;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+
+import Deploying from './Projectmain';
+
+ReactDOM.render(<Deploying />, document.getElementById('root'));
