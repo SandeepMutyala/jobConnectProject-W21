4ba2475b25b2d2231e2cf50cd3ad86aad1375432
Bijitashya <bijit12cse@gmail.com>
2022-03-26 17:36:00 -0300

    user management initial

---
+++ backend/routes/auth.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  registerUser,
+  loginUser,
+  getUserProfile,
+  isAuthenticatedUser,
+  logout,
+} = require("../controllers/authController");
+
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+router.route("/me").get(isAuthenticatedUser, getUserProfile);
+router.route("/logout").get(logout);
+module.exports = router;
---
+++ frontend/src/reducers/userReducer.js
@@ -0,0 +1,103 @@
+import {
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_FAIL,
+  LOGOUT_SUCCESS,
+  LOGOUT_FAIL,
+  CLEAR_ERRORS,
+  LOAD_USER_REQUEST,
+  LOAD_USER_SUCCESS,
+  LOAD_USER_FAIL,
+} from "../constants/userConstants";
+
+export const authReducer = (state = { user: {} }, action) => {
+  switch (action.type) {
+    case LOGIN_REQUEST:
+      return {
+        loading: true,
+        isAuthenticated: false,
+      };
+
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isAuthenticated: true,
+        user: action.payload,
+      };
+
+    case LOGOUT_SUCCESS:
+      return {
+        loading: false,
+        isAuthenticated: false,
+        user: null,
+      };
+
+    case LOAD_USER_REQUEST:
+      return {
+        loading: true,
+        isAuthenticated: false,
+      };
+    case LOAD_USER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isAuthenticated: true,
+        user: action.payload,
+      };
+    case LOAD_USER_FAIL:
+      return {
+        loading: false,
+        isAuthenticated: false,
+        user: null,
+        error: action.payload,
+      };
+
+    case LOGOUT_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+
+    case LOGIN_FAIL:
+      return {
+        ...state,
+        loading: false,
+        isAuthenticated: false,
+        user: null,
+        error: action.payload,
+      };
+    case REGISTER_USER_REQUEST:
+      return {
+        loading: true,
+        isAuthenticated: false,
+      };
+    case REGISTER_USER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isAuthenticated: true,
+        user: action.payload,
+      };
+    case REGISTER_USER_FAIL:
+      return {
+        ...state,
+        loading: false,
+        isAuthenticated: false,
+        user: null,
+        error: action.payload,
+      };
+
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
