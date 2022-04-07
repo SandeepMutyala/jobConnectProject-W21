dfbdc34dbb3ddf3bd55b97965f916c0971e3de8e
Library User <lib-user@libmac20.local>
2022-04-07 20:09:43 -0300

    heroku push

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -127,7 +127,9 @@ exports.forgotPassword = async (req, res, next) => {
 
   await user.save({ validateBeforeSave: false });
 
-  const resetUrl = `${process.env.FRONT_END_URL}/password/reset/${resetToken}`;
+  const resetUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/password/reset/${resetToken}`;
 
   const message = `Your password reset token is as follow:\n\n${resetUrl}\n\nIf you have not requested this email, then ignore it.`;
 
